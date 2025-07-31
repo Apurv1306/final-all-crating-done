ffe9852bb291647635c5b667903bc57f5526c02f
Apurv1306 <156331225+Apurv1306@users.noreply.github.com>
2025-07-31 19:15:13 +0530

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,11 +1,9 @@
-import pytest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app
 
-@pytest.fixture
-def client():
-    with app.test_client() as c:
-        yield c
-
-def test_homepage(client):
-    rv = client.get('/')
-    assert rv.status_code == 200
+def test_homepage():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 200
